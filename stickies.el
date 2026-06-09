72a73355e776d4389d85b41868cced0ad759d2d9
Felix Lange <fjl@twurst.com>
2026-06-09 22:33:27 +0200

    increase internal-border-width of note frame
    On GTK, resizing is handled by the internal border. It's very hard to grab the
    border when the hitbox is only one pixel, so increase it.

--- stickies.el
+++ stickies.el
@@ -967,7 +967,7 @@ target or ATTEMPTS (default 20) is exhausted."
     (drag-internal-border . t)
     (unsplittable . t)
     (vertical-scroll-bars . nil)
-    (internal-border-width . 1)
+    (internal-border-width . 3)
     (menu-bar-lines . 0)
     (tool-bar-lines . 0)
     (tab-bar-lines . 0))
